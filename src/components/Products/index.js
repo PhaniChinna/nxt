29fc106e65a3f44d191a000a4f10cc9a4736cd14
PhaniChinna <phaniyarram@gmail.com>
2022-11-30 05:12:09 +0000

    first commit

---
+++ src/components/Products/index.js
@@ -0,0 +1,18 @@
+import Header from '../Header'
+
+import './index.css'
+
+const Products = () => (
+  <>
+    <Header />
+    <div className="bg2-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+        className="pro-img"
+        alt=" products"
+      />
+    </div>
+  </>
+)
+
+export default Products
